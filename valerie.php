126e24605f857c9a2ae71737082c8e6f8da973bc
Aliye Gallagher <aliyegallagher@gmail.com>
2018-09-24 16:39:57 -0400

    valerie include to PHP and header deleted

--- valerie.php
+++ valerie.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Valerie Tam</title>
-	<link rel="stylesheet" type="text/css" href="css/current.css">
-	<link rel="stylesheet" href="css/styles.css">
-	<link rel="stylesheet" href="css/navigation.css">
-	<link rel="icon" href="img/icon.png">
+<?php include "inc/html-top.inc";?>
+<title>Valerie Tam</title>
 </head>
 <body>
 		<header>
