9d4a13389739b5f5f9c3bc91c9ee33abd0ca1d6f
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-28 21:22:08 -0400

    Updated kevin's code

--- kevin.php
+++ kevin.php
@@ -1,70 +1,100 @@
-<?php include "inc/html-top.inc";?>
-<title>Kevin Kao</title>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kevin Kao</title>
+  <link rel="icon" href="img/icon.png">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/navigation.css">
+  <link rel="stylesheet" type="text/css" href="css/current.css">
 </head>
 
 <body>
 
     <header>
-        <h1>Kevin Kao</h1>
-
+      <h1>Kevin Kao</h1>
+    </header>
 
         <?php include "inc/nav.inc"; ?>
 
-        <figure>
-          <img src="img/line.jpg" alt="section line">
-        </figure>
-        <div class = "kevin">
-          <figure>
-            <img src="img/kevin.jpg" alt="profile-pic">
-          </figure>
-
-          <ul class="kevin">
-            <li>Major: Double major in CS and Mathematics</li>
-            <li>Minor: Phyiscs</li>
-            <li>Graduation: 2021</li>
-          </ul>
-      </div>
-
+       
     </header>
 
 
-    <article>
+    <main>
+       <img src="img/line.jpg" alt="section line">
+      
+        <div class = "kevin">
+          <figure>
+            <img src="img/kevin-headshot.jpg" alt="profile-pic">
 
-      <h2> About Me</h2>
-      <p>
-        Hello there! My name is Kevin Kao. I was born in New York but went to Taiwan for primary and secondary educations! I am currently a student (sophomore) doubling major in Computer Science and Mathematics at the University of Rochester. I have strong interest in the field of science, and I have taken many courses related to my major last year. For example, I took CSC (170, 171, 172) and MTH (150, 165, 171, 172). In addition to that, I am now taking CSC (173, 174, 214) and MTH (173, 190, 201) to better prepare me for my career. Here I would like to share what triggers me to become a copmuter scientist and why I love technology so much!
-      </p>
+            <figcaption>
+              Here's a picture of Kevin.
+            </figcaption>
+         </figure>
+
+        </div>
+
+      <h2 class="title">
+        About Me
+      </h2>
+
+        <p>
+          Hello there! My name is Kevin Kao. I was born in New York, but gre up in Thailand during much of my adolescence! I am currently a sophomore double majoring in Computer Science and Mathematics at the University of Rochester.  Here I would like to share what triggers me to become a computer scientist and why I love technology so much!
+        </p>
 
 
-      <h2 class="title">My Story</h2>
-      <p>
-        When I was four years old, I had severe pneumonia, for which I stayed in the ICU for two weeks. When I was seven, I had an asthma attack on the running track during elementary school. I was taken to the ER to be treated with a nebulizer. In both cases, I would not have survived without the care of doctors, nurses, parents, and more importantly, the advanced medical technology that made treatment possible. Since then, I was determined to improve human life with technology. Technology has saved my life twice.
-      </p>
+      <h2 class="title">
+        Early Life and Education
+      </h2>
 
-      <p>
-        There are many ways that technology can change people’s life. Elon Musk, an entrepreneur who has led the space exploration effort by launching advanced rockets, arguably saved Mother Earth by reducing the fuel usage with electric cars, and envisioned Hyperloop as a fifth transportation whose speed is faster than airline speed, which could significantly reduce travel time. Or consider Mark Zuckerberg, a visionary who with his team has brought people together with the world’s biggest social network and designed a giant drone “Aquila” to make the internet accessible to distant areas. I aspire to become a successful engineer and entrepreneur like them and impact our world positively!
-      </p>
+        <p>
+          When I was four years old, I had severe pneumonia, for which I stayed in the ICU for two weeks. When I was seven, I had an asthma attack on the running track during elementary school. I was taken to the ER to be treated with a nebulizer. In both cases, I would not have survived without the care of doctors, nurses, parents, and more importantly, the advanced medical technology that made treatment possible. Since then, I was determined to improve human life with technology. Technology has saved my life twice.
+        </p>
 
-      <h2 class="title">Education</h2>
+        <p> 
+          I studied in Taiwan for primary and secondary education, before coming to the University of Rochester. I have a strong interest in the field of science, and I have taken many courses related to my majors (Computer Science and Mathematics) last year. For example, I took <em>CSC (170, 171, 172)</em> and <em>MTH (150, 165, 171, 172)</em>. In addition to that, I am now taking <em>CSC (173, 174, 214)</em> and <em>MTH (173, 190, 201)</em> to better prepare me for my career. I am minoring in Physics, and expect to graduate in 2021
+        </p>
 
-      <p>
-        As the pioneer institution in advanced research and innovation, Hajim School of Engineering will be the perfect place for my exploratory journey to become a great engineer. I would expose myself to the many different fields of engineering by taking advantage of the wide selection of courses and learning from the world-class faculty at the School of Engineering. While the exceptional academic program will prepare me with a solid knowledge foundation, the abundant research and internship opportunities would allow me to apply and experiment the knowledge alongside with prominent professors and industry leaders.
-      </p>
+        <p>
+          As the pioneer institution in advanced research and innovation, Hajim School of Engineering will be the perfect place for my exploratory journey to become a great engineer. I would expose myself to the many different fields of engineering by taking advantage of the wide selection of courses and learning from the world-class faculty at the School of Engineering. While the exceptional academic program will prepare me with a solid knowledge foundation, the abundant research and internship opportunities would allow me to apply and experiment the knowledge alongside with prominent professors and industry leaders.
+        </p>
 
-      <p>
-        As the founder of Math Team and member of Engineering Club at my high school, I created a supportive learning environment to tackle interesting math problems and build robots outside of the classroom with peers who shared the same passion. I enjoy putting ideas into actions and learning through peer-led collaboration. I was excited when I have learned about hackathon, a platform for programmers to collaborate with each other and create an interesting project under twenty-four hours! My friend and I went to the hackathon at Syracuse University last semester, and, although the result was not ideal, I made several friends there and gained tons of fun and excitements! I firmly believe that those two days we spent there were worthwhile for my mundane freshman year.
-      </p>
 
-      <h2 class="title">Summary</h2>
+      <h2 class="title">
+        Experience
+      </h2>
 
-      <p>
-        When I think of the University of Rochester, I think of thousands of patients such as myself who has or may suffer from potentially fatal infections or acute asthma attacks. I think of the technology-enabled medical techniques that literally saved millions of lives. I think of an inspiration-filled atmosphere that perpetually battles at the forefront of breakthrough innovation. And I think of a world-class institution where I believe would be the best place for me to study as an engineer and as a leader, allowing me to be bold to dream and to be strong to lead!
-      </p>
-    </article>
+       <p>
+         As the founder of Math Team and member of Engineering Club at my high school, I created a supportive learning environment to tackle interesting math problems and build robots outside of the classroom with peers who shared the same passion. I enjoy putting ideas into actions and learning through peer-led collaboration. I was excited when I have learned about hackathon, a platform for programmers to collaborate with each other and create an interesting project under twenty-four hours! My friend and I went to the hackathon at Syracuse University last semester, and, although the result was not ideal, I made several friends there and gained tons of fun and excitements! I firmly believe that those two days we spent there were worthwhile for my mundane freshman year.
+       </p>
+
+
+      <h2 class="title">
+        Goals
+      </h2>
+
+        <p>
+          When I think of the University of Rochester, I think of thousands of patients such as myself who has or may suffer from potentially fatal infections or acute asthma attacks. I think of the technology-enabled medical techniques that literally saved millions of lives. I think of an inspiration-filled atmosphere that perpetually battles at the forefront of breakthrough innovation. And I think of a world-class institution where I believe would be the best place for me to study as an engineer and as a leader, allowing me to be bold to dream and to be strong to lead! My goal, one day, is to be a computer scientist and engineer, and use technology to help others the same way it helped me!
+        </p>
+    </main>
 
     <footer>
+      <h2 class="title">
+        Source
+      </h2>
+
+      <div>
+        <a href="http://csc174.org/assignment01/kkao4/">Initial Personal Webpage | Kevin</a>
+      </div>
+
+      <img class="img1" src="img/line.jpg" alt="section line">
+      <?php include "inc/nav.inc"; ?>
+      
       <div class="bottom">
-      This webpage, by team Vancouver. Original Source <a href="http://csc174.org/assignment01/kkao4/">Initial Personal Webpage | Kevin</a>
+      This webpage by Team Vancouver.
       </div>
     </footer>
 
